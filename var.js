62b45637718ba76a0b7b48caded449f6bb1c9fe3
nagaganesh05 <somanagaganesh@gmail.com>
2024-10-24 19:26:42 +0530

    variables

---
+++ var.js
@@ -0,0 +1,20 @@
+var a = 30;
+var b = 20;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+let c = 40;
+let d = 30;
+console.log(c+d)
+console.log(c-d)
+console.log(c*d)
+console.log(c/d)
+
+const e = 60;
+const f = 50;
+console.log(e+f)
+console.log(e-f)
+console.log(e*f)
+console.log(e/f)
